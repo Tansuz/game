02030da2dc087bcd8afa29b4e89751560cab51ed
wilcode-taneli <taneli@wilcode.fi>
2022-02-11 15:34:44 +0200

    button homepage alpha

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,24 @@
+// Bootstrap
+import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+
 //Styling
 import './Home.scss'
 
 const Home = () => {
+  const navigate = useNavigate()
+  const handleClickStart = () => navigate('/start')
+
   return (
     <div className='exerciseContainer1'>
-      <h2>home</h2>
+      <p>Peli alkaa painamalla aloita painiketta!</p>
+      <p>Näytöllä oleva sana tulee kirjoittaa siten</p>
+      <p>Ensiksi syötetään ensimmäinen kirjain sitten</p>
+      <p>viimeinen kirjain!</p>
+      <p>Kun kirjainta painetaan poistuu kaikki kyseiset kirjaimet!</p>
+      <Button className='Button' onClick={handleClickStart}>
+        Aloita
+      </Button>
     </div>
   )
 }
